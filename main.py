1b2e52ec2f6017c900e10d54c914e4db31a8b6c4
test <sebastian.kuppa@web.de>
2022-12-24 12:31:19 +0800

    added training data preprocessing

--- main.py
+++ main.py
@@ -2,9 +2,22 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 from torch.autograd import Variable
+from torch.utils.data import DataLoader
 from torchvision import datasets, transforms
 from torchvision.utils import save_image, make_grid
 from torch.utils.tensorboard import SummaryWriter
 
 import argparse
 import numpy as np
+
+batch_size = 32
+
+# data preprocessing
+train_transform = transforms.Compose([
+    transforms.Resize((64, 64)),
+    transforms.ToTensor(),
+    transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])
+    ])
+
+train_dataset = datasets.ImageFolder(root="anime", transform=train_transform,)
+train_loader = DataLoader(train_dataset,batch_size=batch_size, shuffle=True,)
